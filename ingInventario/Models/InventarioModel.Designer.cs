33dbff48dd18683c6c3db389e826302418d7ae44
Darío Irribarra <dario.irribarra.b@gmail.com>
2021-05-20 20:33:00 -0400

    Cambio de servidor

--- ingInventario/Models/InventarioModel.Designer.cs
+++ ingInventario/Models/InventarioModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\workspace\ingInventario\ingInventario\Models\InventarioModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Dario Laptop\Source\Repos\darioipvg\ingInventario\ingInventario\Models\InventarioModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
